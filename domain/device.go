1281e583019dea494e04bb0a5815425a5f3a7f85
Uwem Akan <uwemakan12@gmail.com>
2024-11-14 10:54:07 +0100

    defined domain models and request and response structs

--- domain/device.go
+++ domain/device.go
@@ -1,3 +1,27 @@
 package domain
 
-// TODO: signature device domain model ...
+type SignatureDevice struct {
+	ID               string `json:"id"`
+	Algorithm        string `json:"algorithm"`
+	Label            string `json:"label"`
+	SignatureCounter int    `json:"signatureCounter"`
+	LastSignature    string `json:"lastSignature"`
+	PublicKey        string `json:"-"`
+	PrivateKey       string `json:"-"`
+}
+
+type SignatureDeviceRequest struct {
+	ID        string  `json:"id"`
+	Algorithm string  `json:"algorithm"`
+	Label     *string `json:"label"`
+}
+
+type SignTransactionResponse struct {
+	Signature  string `json:"signature"`
+	SignedData string `json:"signed_data"`
+}
+
+type SignTransactionRequest struct {
+	ID   string `json:"id"`
+	Data string `json:"data"`
+}
